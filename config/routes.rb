2b5ae68f046c65cdb56905339a709a3ada974b40
Filippa Karrfelt <filippakarrfelt@DN0a23373d.SUNet>
2015-11-16 21:54:31 -0800

    added all the html's for all the student pages

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   get 'login' => 'login#index'
   get 'student/landing' => 'student#landing'
   get 'student/review' => 'student#review'
-  get 'student/project_home' => 'student#project_home'
+  get 'student/project_step' => 'student#project_step'
   get 'student/background' => 'student#background'
-  get 'student/suplies' => 'student#supplies'
+  get 'student/supplies' => 'student#supplies'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
