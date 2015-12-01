05ad15935561687d0335abf1109567ef07c07881
iakujobi16 <iakujobi16@gmail.com>
2015-11-30 20:15:59 -0800

    yay steps

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,8 @@ Rails.application.routes.draw do
   get 'login' => 'login#index'
   get 'student/landing' => 'student#landing'
   get 'student/review' => 'student#review'
-  get 'student/project_step/:id' => 'student#project_step'
-  # get 'student/project/:project_id/project_step/:id' => 'student#project_step'
+  # get 'student/project_step/:id' => 'student#project_step'
+  get 'student/project/:project_id/project_step/:id' => 'student#project_step'
 
 
   get 'student/background' => 'student#background'
