6540f76791c3c99b3638bd3647ae28e27bc9bfe6
Priyanka Rao <prao96@stanford.edu>
2015-12-02 22:37:24 -0800

    Dynamic rendering works

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,16 @@ Supply.create({project_id: 1, supply: 'Salt'})
 Step.create({project_id: 1, step: 'In this step we will dissolve salt in water. Add 1000mL of water in to your large container. Add 10g of salt into the container. Use spoon to mix salt in.', step_number: 1})
 Step.create({project_id: 1, step: 'Hey this is a test.', step_number: 2})
 
+Project.create({school_class_id: 1, project_id: 2, title: 'Building a Responsive Web App', description: 'Have you ever wanted to build your own web app? Lucky you, this project walks you through that process.', landing_image: '<img src="http://www.bigfoto.com/
+	miscellaneous/photos-16/salt-crystals-94jf.jpg" alt="Mountain View" style="width:3
+	04px;height:228px;">', background_image: '<img src="https://lhmrussia.files.wordpress.com/201
+	5/04/nacl.jpg" alt="NaCl" style="width:400px;height:228px;">', summary: 'You just learned something new. Congrats!', number_of_steps: 4})
+Supply.create({project_id: 2, supply: 'Computer'})
+Supply.create({project_id: 2, supply: 'Brain Juice'})
+Supply.create({project_id: 2, supply: 'Access to internet'})
+Step.create({project_id: 2, step: 'Get some familiarity with HTML/CSS and Bootstrap. Responsive web apps use different stylesheets on different devices.', step_number: 1})
+Step.create({project_id: 2, step: 'Get familiar with the Rails web framework. It simplifies web dev, A LOT.', step_number: 2})
+Step.create({project_id: 2, step: 'Build your web app (this is the hard part, we know). Use StackOverflow!!', step_number: 3})
+Step.create({project_id: 2, step: 'Use heroku to host your web app so everyone can see it. Make sure to precompile your assets!', step_number: 4})
+
+
